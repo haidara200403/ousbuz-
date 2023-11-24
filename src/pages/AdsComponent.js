d9aa00436cf927f1db32183db90fa098f972e33e
haidara200403 <88344323+haidara200403@users.noreply.github.com>
2023-11-24 13:22:53 +0100

    Update AdsComponent.js

--- src/pages/AdsComponent.js
+++ src/pages/AdsComponent.js
@@ -23,7 +23,7 @@ const AdsComponent = (props) => {
         <>
             <ins className="adsbygoogle"
                 style={{ display: "block" }}
-                data-ad-client="ca-pub-XXXXXXXXXXXXXXX"
+                data-ad-client="ca-pub-2079820680072913"
                 data-ad-slot={dataAdSlot}
                 data-ad-format="auto"
                 data-full-width-responsive="true">
